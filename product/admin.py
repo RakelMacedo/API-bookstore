53fef4b368476292e2c11a5dff517bac7f55f9fd
Rakel <macedorakel35@gmail.com>
2022-08-24 21:45:24 -0300

    updating registers in admin

--- product/admin.py
+++ product/admin.py
@@ -3,3 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models.category import Category
 from .models.product import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "description")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "active")
+
+
+admin.site.register(Product, ProductAdmin)
