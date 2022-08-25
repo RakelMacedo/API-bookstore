dcb951612d19298733d303f1519a52f0fbf62e32
Rakel <macedorakel35@gmail.com>
2022-08-24 21:46:21 -0300

    update models

--- product/models/product.py
+++ product/models/product.py
@@ -5,7 +5,10 @@ from .category import Category
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=600, blank=True, null=True)
+    description = models.TextField(max_length=900, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return self.title
