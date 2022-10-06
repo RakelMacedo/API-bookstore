7f03747f744d3eff9cb90281ced48a03b5a22e46
Rakel <macedorakel35@gmail.com>
2022-10-06 13:35:55 -0300

    removing heroku

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-7s2v$nkqy02%j(j-ou7lr^k7vpb_ar3z7$h6*$_gd12efu$1&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "api-bookstore-python.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
